20d564f5bc035fe27cc00d3ab6fff58ad3794f9f
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-26 10:54:48 -0400

    Separate post fetching into its own module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-const app = (async () => {
-  let postContainer = document.createElement('div');
-  
-  const posts = await fetch('http://localhost:3000/api/posts')
-    .then(res => res.json())
-    .catch(err => console.log(err));
-  
-  posts.forEach(post => {
-    let container = document.createElement('div'),
-        postTitle = document.createElement('h1'),
-        postContent = document.createElement('p'),
-        postTime = document.createElement('p');
-    
-    container.classList.add('post');
+import posts from './posts';
 
-    postTitle.innerText = post.title;
-    postContent.innerText = post.content;
-    postTime.innerText = post.time.created;
-
-    container.append(postTitle, postContent, postTime);
-    postContainer.appendChild(container);
-  });
-
-  postContainer.classList.add('container');
-  document.body.appendChild(postContainer);
+const app = (() => {
+  document.body.appendChild(posts);
 })();
