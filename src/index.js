534b284feb83a529e2f7487eedfd243b9f8d2d3f
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-25 22:51:22 -0400

    Add function to script to fetch and display all blog posts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-const app = (() => {
-  const welcome = document.createElement('h1');
-  welcome.innerText = 'Welcome to my blog!';
+const app = (async () => {
+  let postContainer = document.createElement('div');
+  
+  const posts = await fetch('http://localhost:3000/api/posts')
+    .then(res => res.json())
+    .catch(err => console.log(err));
+  
+  posts.forEach(post => {
+    let container = document.createElement('div'),
+        postTitle = document.createElement('h1'),
+        postContent = document.createElement('p'),
+        postTime = document.createElement('p');
+    
+    container.classList.add('post');
 
-  document.body.appendChild(welcome);
+    postTitle.innerText = post.title;
+    postContent.innerText = post.content;
+    postTime.innerText = post.time.created;
+
+    container.append(postTitle, postContent, postTime);
+    postContainer.appendChild(container);
+  });
+
+  postContainer.classList.add('container');
+  document.body.appendChild(postContainer);
 })();
