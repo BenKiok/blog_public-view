7a5b3a99460aded31a89acac60225ed93578e856
Benedict Kioko <benedictmkioko@gmail.com>
2022-03-30 23:32:19 -0400

    Simplify variables in returnPost function

--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,16 @@
 const returnPost = post => {
 	let container = document.createElement('div'),
-			postTitle = document.createElement('h1'),
-			postContent = document.createElement('p'),
-			postTime = document.createElement('p');
+			title = document.createElement('h1'),
+			content = document.createElement('p'),
+			time = document.createElement('p');
 	
 	container.classList.add('post');
 
-	postTitle.innerText = post.title;
-	postContent.innerText = post.content;
-	postTime.innerText = post.time.created;
-
-	container.append(postTitle, postContent, postTime);
+	title.innerText = post.title;
+	content.innerText = post.content;
+	time.innerText = post.time.created;
+	
+	container.append(title, content, time);
 	return container;
 }
 
