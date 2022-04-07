76241e90935effaa6c0c3bd9e9a2bd502dea795d
Benedict Kioko <benedictmkioko@gmail.com>
2022-04-07 00:42:43 -0400

    Add working comment form for single post view

---
+++ src/commentForm.js
@@ -0,0 +1,65 @@
+import { returnComments } from "./functions";
+
+const commentForm = id => {
+  const form = document.createElement('form'),
+        usernameLabel = document.createElement('label'),
+        usernameInput = document.createElement('input'),
+        contentLabel = document.createElement('label'),
+        contentInput = document.createElement('input'),
+        button = document.createElement('button');
+
+  usernameLabel.innerText = 'Username';
+  usernameInput.type = 'text';
+  usernameInput.name = 'username';
+  usernameInput.placeholder = 'johndoe123';
+  usernameInput.required = true;
+  contentLabel.innerText = 'Comment';
+  contentInput.type = 'text';
+  contentInput.name = 'content';
+  contentInput.placeholder = 'Wow! Great article!';
+  contentInput.required = true;
+  button.innerText = 'Post';
+
+  usernameLabel.appendChild(usernameInput);
+  contentLabel.appendChild(contentInput);
+  form.append(usernameLabel, contentLabel, button);
+
+  button.addEventListener('click', event => {
+    event.preventDefault();
+
+    if (usernameInput.value.length && contentInput.value.length) {
+      (async () => {
+        const commentData = {
+          username: usernameInput.value,
+          content: contentInput.value,
+          timeCreated: Date()
+        }
+
+        await fetch('http://localhost:3000/api/posts/' + id + '/comments/new',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(commentData)
+          }
+        )
+        .catch(err => console.log(err));
+
+        usernameInput.value = '';
+        contentInput.value = '';
+
+        const commentArr = await fetch('http://localhost:3000/api/posts/' + id + '/comments')
+          .then(res => res.json())
+          .catch(err => console.log(err));
+        
+        Array.from(document.body.childNodes)[3].remove();
+        document.body.appendChild(returnComments(commentArr));
+      })();
+    }
+  });
+
+  return form;
+}
+
+export default commentForm;
